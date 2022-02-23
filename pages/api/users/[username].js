60709e33b81e92a4b13c02e62239ec4597526331
emremutlu08 <mutluemre93@gmail.com>
2022-02-23 22:02:15 +0300

    Messages Updated

--- pages/api/users/[username].js
+++ pages/api/users/[username].js
@@ -4,18 +4,19 @@ import Users from '../../../models/Users';
 
 /* MESSAGES */
 import {
-  ITEM_LISTED,
-  ITEM_LISTED_ERROR,
-  ITEM_EDITED,
-  ITEM_EDITED_ERROR,
-  ITEM_DELETED,
-  ITEM_DELETED_ERROR,
+  FILL_AREAS,
   WRONG_METHOD,
-} from '../../../lib/api/projects/messages';
+  UNAUTHORIZED, // TODO: CHANGE TO UNAUTHORIZED
+} from '../../../lib/general/messages';
+import {
+  USER_LISTED,
+  USER_LISTED_ERROR,
+  USER_DELETED,
+  USER_DELETED_ERROR,
+} from '../../../lib/api/users/messages';
 
 /* MAIN FUNCTION */
 export default async function handler(req, res) {
-  let { body } = req;
   const {
     query: { username },
     method,
@@ -25,44 +26,26 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET' /* Get a model by its ID */:
       try {
-        const galleryItem = await Users.findOne({ username });
+        const userItem = await Users.findOne({ username });
 
-        if (!galleryItem) {
+        if (!userItem) {
           return res.status(400).json({
             success: false,
-            message: ITEM_LISTED_ERROR,
+            message: USER_LISTED_ERROR,
             loading: false,
           });
         }
 
         res.status(200).json({
           success: true,
-          data: galleryItem,
-          message: ITEM_LISTED,
+          data: userItem,
+          message: USER_LISTED,
           loading: false,
         });
       } catch (error) {
         res
           .status(400)
-          .json({ success: false, message: ITEM_LISTED_ERROR, loading: false });
-      }
-      break;
-
-    case 'PUT' /* Edit a model by its ID */:
-      try {
-        const updatedData = { ...body };
-        // update an item
-        await Users.findOneAndUpdate({ username }, updatedData);
-        res.status(200).json({
-          success: true,
-          data: updatedData,
-          message: ITEM_EDITED,
-          loading: false,
-        });
-      } catch (error) {
-        res
-          .status(400)
-          .json({ success: false, message: ITEM_EDITED_ERROR, loading: false });
+          .json({ success: false, message: USER_LISTED_ERROR, loading: false });
       }
       break;
 
@@ -72,13 +55,13 @@ export default async function handler(req, res) {
         res.status(200).json({
           success: true,
           data: {},
-          message: ITEM_DELETED,
+          message: USER_DELETED,
           loading: false,
         });
       } catch (error) {
         res.status(400).json({
           success: false,
-          message: ITEM_DELETED_ERROR,
+          message: USER_DELETED_ERROR,
           loading: false,
         });
       }
